7631f1d606693eba505b31a8c5b06e9ca6377451
Juan Balceda <juanbalceda@gmail.com>
2020-10-04 19:43:09 +0200

    Added lumen-passport

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -99,7 +99,10 @@ $app->configure('app');
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
+
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
