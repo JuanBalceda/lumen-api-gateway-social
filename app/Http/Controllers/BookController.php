99806830636d274c791097bd4c4630115eb9e7a5
Juan Balceda <juanbalceda@gmail.com>
2020-10-04 19:36:35 +0200

    Updated books controller and service

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,70 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
     use ApiResponse;
 
     public $bookService;
+    public $authorService;
 
     /**
      * Create a new controller instance.
      *
      * @param BookService $bookService
-     * @return void
+     * @param AuthorService $authorService
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     /**
      * Return book list
-     * @param Request $request
-     * @return void
+     * @return Response
      */
-    public function index(Request $request)
+    public function index()
     {
+        return $this->successResponse($this->bookService->findAll());
     }
 
     /**
      * Create an instance of Book
      * @param Request $request
-     * @return void
+     * @return Response
      */
     public function store(Request $request)
     {
+        $this->authorService->findById($request->author_id);
+
+        return $this->successResponse($this->bookService->create($request->all()), Response::HTTP_CREATED);
     }
 
     /**
      * Return an instance of Book
      * @param int $idBook
-     * @return void
+     * @return Response
      */
     public function show($idBook)
     {
+        return $this->successResponse($this->bookService->findById($idBook));
     }
 
     /**
      * Update an specific book
      * @param Request $request
      * @param int $idBook
-     * @return void
+     * @return Response
      */
     public function update(Request $request, $idBook)
     {
+        return $this->successResponse($this->bookService->update($request->all(), $idBook));
     }
 
     /**
      * Delete an instance of Book
      * @param int $idBook
-     * @return void
+     * @return Response
      */
     public function destroy($idBook)
     {
+        return $this->successResponse($this->bookService->delete($idBook));
     }
 }
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -20,4 +20,55 @@ class BookService
     {
         $this->baseUri = config('services.books.baseUri');
     }
+
+    /**
+     * Retrieve full list of books
+     * @return string
+     */
+    public function findAll()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+
+    /**
+     * Create a new book
+     * @param array $data
+     * @return string
+     */
+    public function create($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    /**
+     * Retrieve book by id
+     * @param int $idBook
+     * @return string
+     */
+    public function findById($idBook)
+    {
+        return $this->performRequest('GET', "/books/{$idBook}");
+    }
+
+    /**
+     * Update an book
+     * @param array $data
+     * @param int $idBook
+     * @return string
+     */
+    public function update($data, $idBook)
+    {
+        return $this->performRequest('PUT', "/books/{$idBook}", $data);
+    }
+
+    /**
+     * Update an book
+     * @param int $idBook
+     * @return string
+     */
+    public function delete($idBook)
+    {
+        return $this->performRequest('DELETE', "/books/{$idBook}");
+    }
+
 }
