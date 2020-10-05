e8fbf85b218dc31f8e64f0533ec2ddd0b518e1ce
Juan Balceda <juanbalceda@gmail.com>
2020-10-05 23:14:00 +0200

    Improved SocialAuthController

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Traits\ApiResponse;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 use phpseclib\Crypt\Random;
@@ -27,20 +28,37 @@ class SocialAuthController extends Controller
         $social_user = Socialite::driver($provider)->stateless()->user();
 
         if ($user = User::where('email', $social_user->email)->first()) {
-            return $this->generateAccessToken($provider, $user);
+
+            $oauth_access_token = $user->tokens()
+                ->where('name', $provider)
+                ->whereDate('expires_at', '>', Carbon::now())
+                ->first();
+
+            if (isset($oauth_access_token)) {
+                $access_token = $oauth_access_token->id;
+            } else {
+                $access_token = $user->createToken($provider)->accessToken;
+            }
+
+            return $this->generateAccessToken($access_token, $user);
         } else {
+
             $user = User::create([
                 'name' => $social_user->name,
                 'email' => $social_user->email,
                 'password' => Hash::make(Random::string(32))
             ]);
-            return $this->generateAccessToken($provider, $user);
+
+            $access_token = $user->createToken($provider)->accessToken;
+
+            return $this->generateAccessToken($access_token, $user);
         }
     }
 
-    public function generateAccessToken($provider, $user)
+    public function generateAccessToken($access_token, $user)
     {
-        $data['access_token'] = $user->createToken($provider)->accessToken;
+        $data['user'] = $user;
+        $data['access_token'] = $access_token;
 
         return $this->validResponse($data);
     }
